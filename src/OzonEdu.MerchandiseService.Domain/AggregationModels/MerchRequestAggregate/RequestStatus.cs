5581f3069a8fa9c2c337b5283c13030655ea4690
Шевченко Нина <shev.nancy@gmail.com>
2021-11-05 00:14:47 +0300

    [30.10] TDD

--- src/OzonEdu.MerchandiseService.Domain/AggregationModels/MerchRequestAggregate/RequestStatus.cs
+++ src/OzonEdu.MerchandiseService.Domain/AggregationModels/MerchRequestAggregate/RequestStatus.cs
@@ -5,7 +5,7 @@ namespace OzonEdu.MerchandiseService.Domain.AggregationModels.MerchRequestAggreg
     public class RequestStatus : Enumeration
     {
         public static RequestStatus Wait = new(1, "Wait");
-        public static RequestStatus Done = new(1, "Done");
+        public static RequestStatus Done = new(2, "Done");
         
         public RequestStatus(int id, string name) : base(id, name)
         {
---
+++ tests/OzonEdu.MerchandiseService.Domain.Tests/MerchRequestTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchRequestAggregate;
+using Xunit;
+
+namespace OzonEdu.MerchandiseService.Domain.Tests
+{
+    public class MerchRequestTests
+    {
+        [Fact]
+        public void ChangeStatusTest()
+        {
+            //Arrange 
+            var merchRequest = new MerchRequest(
+                new Employee(123),
+                new Sku(1234),
+                new RequestDate(DateTime.Now),
+                merchPack: null,
+                RequestStatus.Wait);
+
+
+            //Act
+            merchRequest.ChangeStatus(RequestStatus.Done);
+
+            //Assert
+            Assert.Equal(RequestStatus.Done, merchRequest.RequestStatus);
+        }
+    }
+}
